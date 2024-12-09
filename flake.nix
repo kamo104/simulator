8d71475eaf5b483c1e310520ab8228ae4b6023ca
kamo104 <kamo.gabo@gmail.com>
2024-12-09 16:56:33 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "simulator dev flake";
+  inputs = {
+    nixpkgs.url = "tarball+https://codeload.github.com/NixOS/nixpkgs/tar.gz/e2605d0744c2417b09f8bf850dfca42fcf537d34";
+    flake-utils = {
+      url = "https://codeload.github.com/numtide/flake-utils/tar.gz/11707dc2f618dd54ca8739b309ec4fc024de578b";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs =  with pkgs; [
+            clang-tools
+            boost
+            bear
+            # binutils
+            cmake
+            gnumake
+          ];
+        };
+    });
+}
+
+
+
+
